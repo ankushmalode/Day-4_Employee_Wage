534ced972b5c86b1698d343c0ea2a9cc6f9621b9
ankushmalode <you@example.com>
2021-09-26 15:31:27 +0530

    UC6

--- src/main/java/Employee_Wage.java
+++ src/main/java/Employee_Wage.java
@@ -1,10 +1,10 @@
-import java.util.Random;
 public class Employee_Wage {
-    public static void main(String args[]){
-        int min=0, max=2, rate=20, empHrs=0, totalSalary=0, count=0, fullDayHr=8, partTimeHr=8;
+    public static void main(String[] args){
+        int min=0, max=2, rate=20, empHrs=0, totalSalary=0, count=0;
         for (int i=1; i<=20; i++) {
             int check = (int)(Math.random()*(max-min+1)+min);
-            switch (check) {
+            if (empHrs<=100) {
+                switch (check) {
                     case 1:
                         empHrs= empHrs+8;
                         break;
@@ -14,10 +14,14 @@ public class Employee_Wage {
                     default:
                         empHrs=empHrs+0;
                 }
+            }
                 count = count+1;
         }
         System.out.println("Employee total working hour: "+empHrs);
         System.out.println("Employee total working days: "+count);
 
+        int salary= empHrs*rate;
+        totalSalary= salary+totalSalary;
+        System.out.print("\nTotal salary is "+ totalSalary);
     }
 }
