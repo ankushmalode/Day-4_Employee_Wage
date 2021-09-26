006fcaa12214e50a24a11021dcbe50c571ce5b6f
ankushmalode <you@example.com>
2021-09-26 14:14:00 +0530

    UC1

---
+++ src/main/java/Employee_Wage.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class Employee_Wage {
+    public static void main(String args[]){
+        int min=0, max=2;
+        int check = (int)(Math.random()*(max-min+1)+min);
+        System.out.println(check);
+    }
+}
