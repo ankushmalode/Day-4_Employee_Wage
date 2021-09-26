ab33d1710e04586ff17f250f1ccf613ee6483a71
ankushmalode <you@example.com>
2021-09-26 14:23:35 +0530

    second push UC2 and UC3

--- src/main/java/Employee_Wage.java
+++ src/main/java/Employee_Wage.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 public class Employee_Wage {
     public static void main(String args[]){
-        int min=0, max=2;
+        int min=0, max=2, rate=20, empHrs=0, totalSalary=0, count=0, fullDayHr=8, partTimeHr=8;
         int check = (int)(Math.random()*(max-min+1)+min);
         System.out.println(check);
     }
